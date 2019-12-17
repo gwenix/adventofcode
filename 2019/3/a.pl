f72624323f897db010fce3c6c80464d262debc4f
Gwendolyn Schmidt <gwenix@gmail.com>
2019-12-17 17:19:11 -0500

    finished day 3 part a

--- 2019/3/a.pl
+++ 2019/3/a.pl
@@ -18,15 +18,23 @@ chomp @lines;
 close (F);
 
 my %grid;
+$grid{smX}=0;
+$grid{smY}=0;
+$grid{lgX}=0;
+$grid{lgY}=0;
 my $least_dist=0;
 
 for (my $i=0; $i<@lines; $i++) {
+  my $wire = $i+1;
+  print "New Wire: $wire\n";
+  print "------------\n";
   my ($x, $y) = (0, 0);
-  $grid{$x}{$y}=1;
+  $grid{$x}{$y}="O";
 
   my @dir = split /,/, $lines[$i];
   for (my $j=0; $j<@dir; $j++) {
     my $d = $dir[$j];
+    #print "$d -- \t";
 
     my @foo = split //, $d;
     my $ch = shift @foo;
@@ -44,22 +52,27 @@ for (my $i=0; $i<@lines; $i++) {
       } else {
         die "Unknown direction: $ch\n";
       }
-      if ($grid{$x}{$y}) {
+      if ($grid{$x}{$y} && $grid{$x}{$y}!=$wire) {
+	$grid{$x}{$y}="X";
 	my $dist = abs($x) + abs ($y);
 	$least_dist=$dist if ($dist<$least_dist || $least_dist==0);
+	print "X $x, $y, dist $dist; Least: $least_dist\n";
       } else {
-	$grid{$x}{$y}=1;
+	$grid{$x}{$y}=$wire;
       }
 
-      $grid{size}=$x if ($x>$grid{size});
-      $grid{size}=$y if ($y>$grid{size});
+      $grid{smX}=$x if ($x<$grid{smX} || $grid{smX}==0);
+      $grid{smY}=$y if ($y<$grid{smY} || $grid{smY}==0);
+      $grid{lgX}=$x if ($x>$grid{lgX} || $grid{lgX}==0);
+      $grid{lgY}=$y if ($y>$grid{lgY} || $grid{lgY}==0);
     }
+    #print "Now at $x, $y\n";
   }
 }
 
-Print_Grid(\%grid);
+#Print_Grid(\%grid);
 
-print "First crossing: $least_dist\n";
+#print "First crossing: $least_dist\n";
 
 exit; 
 
@@ -70,10 +83,11 @@ exit;
 sub Print_Grid {
   my %grid = %{shift()};
 
-  for (my $j=$grid{size}; $j>=0-$grid{size}; $j--) {
-    for (my $i=0-$grid{size}; $i<=$grid{size}; $i++) {
+  print "Dimensions: $grid{smX} - $grid{lgX}, $grid{smY} - $grid{lgY}\n";
+  for (my $j=$grid{lgY}; $j>=$grid{smY}; $j--) {
+    for (my $i=0-$grid{smX}; $i<=$grid{lgX}; $i++) {
       my $pt = ".";
-      $pt = "+" if ($grid{$i}{$j});
+      $pt = $grid{$i}{$j} if ($grid{$i}{$j});
       print "$pt";
     }
     print "\n";
